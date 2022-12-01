0d3d0b54b90e77e455778763eec0d5aae1035d36
Eric Websmith <eric.websmith@gmail.com>
2022-12-01 21:53:01 +0800

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template
+
 from app import myapp
+
+posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
+
 
 @myapp.route('/')
 @myapp.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html', title='Home', user={'username': 'Eric'}, posts=posts)
+
+
+@myapp.route('/1')
+def index1():
+    return render_template('index.html', user={'username': 'Eric'})
