ec2585ff2e2a389ac867bb4bbc78baf581ec6cff
Eric Websmith <zhouweiguang@hotmail.com>
2022-12-02 14:36:57 +0800

    4: Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.config import Config
-import os
+
 myapp = Flask(__name__)
-SECRET_KEY = os.urandom(32)
-myapp.config['SECRET_KEY'] = SECRET_KEY
-# myapp.config.from_object(Config)
-from app import routes
+# SECRET_KEY = os.urandom(32)
+# myapp.config['SECRET_KEY'] = SECRET_KEY
+myapp.config.from_object(Config)
+
+db = SQLAlchemy(myapp)
+migrate = Migrate(myapp, db)
+from app import routes, models
--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import myapp
+from app import db, myapp
+from app.models import User, Post
+
+@myapp.shell_context_processor
+def make_content_processor():
+    return {'db': db, 'User': User, 'Post': Post}
+
