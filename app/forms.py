a23fcdcbe101342f14cd36e04980a1c87f32c2e7
Eric Websmith <zhouweiguang@hotmail.com>
2022-12-06 11:26:39 +0800

    8: Followers

--- app/forms.py
+++ app/forms.py
@@ -48,3 +48,6 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.original_username).first()
             if user is not None:
                 raise ValidationError('Please use a different username')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
