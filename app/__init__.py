45af54689f3aec9866a8b5e29c1dfae75150e19c
Eric Websmith <zhouweiguang@hotmail.com>
2022-12-07 10:29:39 +0800

    11: Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from app.config import Config
+from flask_bootstrap import Bootstrap
 
 myapp = Flask(__name__)
 SECRET_KEY = os.urandom(32)
@@ -20,6 +21,7 @@ migrate = Migrate(myapp, db)
 login = LoginManager(myapp)
 login.login_view = 'login'
 mail = Mail(myapp)
+bootstrap = Bootstrap(myapp)
 from app import errors, models, routes
 
 if not myapp.debug:
